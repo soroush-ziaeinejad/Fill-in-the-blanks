2f53d0eb02be88f272f420b006fb0ef4d6cc9438
Soroush Ziaeinejad <sz.1994@yahoo.com>
2015-07-01 06:35:31 +0330

    final Phase of AI project

--- Program.py
+++ Program.py
@@ -7,8 +7,8 @@ import ngram
 from nltk.probability import LidstoneProbDist,WittenBellProbDist
 import nltk
 
-def TaggingBlank(input):
-    if(PreBlank(input)[0]=='and' or PreBlank(input)[0]=='or'):
+def TaggingBlank(input):    # determine the tag of blank word in sentence
+    if(PreBlank(input)[0]=='and' or PreBlank(input)[0]=='or'):       # if we have "and" or "or" between blank and previous or next word the tag of blank is the same with that word
         return PrePreBlank(input)[1]
     elif(PostBlank(input)[0]=='and' or PostBlank(input)[0]=='or'):
         return PostPostBlank(input)[1]
@@ -24,9 +24,22 @@ def file_len(fname):
         for i, l in enumerate(f):
             pass
     return i + 1
-
-
-def ngrams(input, n):
+def WhereIsBlank(tagged):
+    array = []
+    for i in range(len(tagged)-1):
+        if ((tagged[i][1] == 'PRP') and tagged[i+1][1] == 'DT'):
+            array.append(tagged[i][0])
+            array.append('$')
+        elif (tagged[i][1] == 'PRP$') and (tagged[i+1][1] == 'VBZ' or tagged[i+1][1] == 'VBP' or tagged[i+1][1] == 'VBD' or tagged[i+1][1] == 'VBN'):
+            array.append(tagged[i][0])
+            array.append('$')
+        elif (tagged[i][0] == 'Please' or tagged[i][0] == 'please') and not ((tagged[i+1][1] == 'VBZ' or tagged[i+1][1] == 'VBP' or tagged[i+1][1] == 'VBD' or tagged[i+1][1] == 'VBN')):
+            array.append(tagged[i][0])
+            array.append('$')
+        else:
+            array.append(tagged[i][0])
+    return array
+def ngrams(input, n):       # ngram function!
   input = input.split(' ')
   output = {}
   for i in range(len(input)-n+1):
@@ -34,30 +47,30 @@ def ngrams(input, n):
     output.setdefault(g, 0)
     output[g] += 1
   return output
-def Verb(input):
+def Verb(input):    # return verb of sentence
     for i in input:
         if (i[1]=='VBP' or i[1]=="VBD"):
             return i[0]
-def PreBlank(input):
+def PreBlank(input): # return the prevoius word of blank
     for i in range(len(input)):
         if (input[i][0]=="$"):
             return input[i-1]
-def PostBlank(input):
+def PostBlank(input):  # return the next word of blank
     for i in range(len(input)):
         if (input[i][0]=="$"):
             return input[i+1]
 
-def PostPostBlank(input):
+def PostPostBlank(input): # next of next of blank
     for i in range(len(input)):
         if (input[i][0]=="$"):
             return input[i+2]
 
-def PrePreBlank(input):
+def PrePreBlank(input): # pre of pre of blank
     for i in range(len(input)):
         if (input[i][0]=="$"):
             return input[i-2]
 
-def TypeOfBlankIsNoun(pre,post):
+def TypeOfBlankIsNoun(pre,post):  # is blank Noun?
     if (pre[1]=="ADJ"):
         return True
     elif (pre[0][-2::]=="'s"):
@@ -67,16 +80,16 @@ def TypeOfBlankIsNoun(pre,post):
     elif (pre[0] in ezafe):
         return True
 
-def TypeOfBlankIsADV(pre,post):
+def TypeOfBlankIsADV(pre,post):   # is blank Adverb?
     if (pre[1]=="VBP"):
         return True
 
-def TypeOfBlankIsADJ(pre,post):
+def TypeOfBlankIsADJ(pre,post):   # is blank Adjective?
     if (post[1]=="NN"):
         return True
     elif (pre[0] in ['is','am','are','was','were']):
         return True
-def TypeOfBlankIsVerb(pre,post):
+def TypeOfBlankIsVerb(pre,post):    # is blank verb?
     if (pre[1]=="NN" or pre[1]=="PRP"):
         return True
     elif(post[0] in ezafe):
@@ -86,16 +99,20 @@ def TypeOfBlankIsVerb(pre,post):
 sentence = input("Enter the sentence and obtain the blank with a '$': ")
 ezafe=['the','a','an','about','after','along','as','at','befor','ebehind','between','by','down','during','except','for','from','in','of','on','with','without']
 tagged = nltk.pos_tag(nltk.word_tokenize(sentence))
-#print (tagged)
+sentence2 = WhereIsBlank(tagged)
+sentence3 = ''.join(sentence2)
+print (sentence2)
+tagged = nltk.pos_tag(nltk.word_tokenize((sentence2)))
+print (tagged)
 verb = Verb(tagged)
 pre , post = PreBlank(tagged),PostBlank(tagged)
-#print (TaggingBlank(tagged))
+print (TaggingBlank(tagged))
 finalTag = TaggingBlank(tagged)
 N=1000
 lines=[]
 f= open("miniTrain.txt","r")
 s=''
-#print (len(open("miniTrain.txt").readlines()))
+print (len(open("miniTrain.txt").readlines()))
 for i in range(N):
     line=f.readline()
     lines.append(line)
@@ -111,6 +128,7 @@ ngrams.sort()
 ls=[]
 for j in range(10):
     tagged2 = nltk.pos_tag(nltk.word_tokenize(ngrams[j][0]))
+    print (tagged2)
     for k in tagged2:
         if k[1]==finalTag:
             ls.append(k[0])
